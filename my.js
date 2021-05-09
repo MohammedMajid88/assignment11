b457a14492b543fa32c1bc261575ad4be65de031
MohammedMajid88 <mohamedmajed8888@gmail.com>
2021-05-09 18:51:47 +0300

    the task not completed yet

---
+++ my.js
@@ -0,0 +1,65 @@
+console.log(getOdd([1, 2, 3, 4, 5, 6]));
+
+function isEven(number) {
+ 
+    return number % 2 === 0;
+  }
+  
+
+
+
+ 
+  function getOdd(number) {
+    const result = [];
+
+    for (let i =  0; i  <  number.length; i++) {
+      if (unmber[i] % 2 !== 0)  {
+        result.push(number[i]);
+
+    }
+  }
+
+  return result;
+}
+
+
+
+
+const message = 'hello';
+foo();
+
+function foo() {
+	let message = 'hi'
+	alert(message);
+}
+
+here the function is taken what is inside the function First if the function title is not found you go to above to search.
+
+
+foo();
+
+const message = 'hello';
+
+function foo() {
+ 
+	alert(message);
+}
+
+
+ here see  Cannot access 'message' before initialization so if (const message = 
+  'hello') inside function  this will be corrict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
